b43ddfa63e532955bdba8cb1bcc8be7514e7372e
Naz <bluspringindohq@gmail.com>
2023-03-14 06:05:17 +0800

    Initial stuff

---
+++ src/main/kotlin/xyz/bluspring/onthequest/OnTheQuest.kt
@@ -0,0 +1,13 @@
+package xyz.bluspring.onthequest
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class OnTheQuest : JavaPlugin() {
+    override fun onEnable() {
+
+    }
+
+    override fun onDisable() {
+
+    }
+}
