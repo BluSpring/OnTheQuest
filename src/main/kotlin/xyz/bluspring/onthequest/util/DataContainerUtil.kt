ff55607e72473feb7cc8e2b867c05c7c43134b8f
Naz <bluspringindohq@gmail.com>
2023-03-17 12:01:52 +0800

    I should probably watch for that

--- src/main/kotlin/xyz/bluspring/onthequest/util/DataContainerUtil.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/util/DataContainerUtil.kt
@@ -2,11 +2,18 @@ package xyz.bluspring.onthequest.util
 
 import org.bukkit.NamespacedKey
 import org.bukkit.persistence.PersistentDataContainer
+import org.bukkit.persistence.PersistentDataType
 
 object DataContainerUtil {
     fun parseKeys(key: NamespacedKey, container: PersistentDataContainer): List<NamespacedKey> {
         if (!container.has(key))
             return emptyList()
+
+        if (container.has(key, PersistentDataType.STRING)) {
+            val keyString = container.get(key, PersistentDataType.STRING)!!
+
+            return listOf(NamespacedKey.fromString(keyString)!!)
+        }
 
         val keyList = container.get(key, StringArrayDataType()) ?: return emptyList()
 
