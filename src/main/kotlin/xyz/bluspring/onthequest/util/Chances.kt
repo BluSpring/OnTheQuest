501e58b9be60b79447c861f3056b651deb1b896c
Naz <bluspringindohq@gmail.com>
2023-03-19 11:37:15 +0800

    Lower chances even further

--- src/main/kotlin/xyz/bluspring/onthequest/util/Chances.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/util/Chances.kt
@@ -1,8 +1,8 @@
 package xyz.bluspring.onthequest.util
 
 object Chances {
-    const val SUPER_LOW = 1.0 / 80.0
-    const val LOW = 1.0 / 50.0
-    const val MEDIUM = 1.0 / 30.0
-    const val HIGH = 1.0 / 15.0
+    const val SUPER_LOW = 1.0 / 50.0
+    const val LOW = 1.0 / 20.0
+    const val MEDIUM = 1.0 / 10.0
+    const val HIGH = 1.0 / 5.0
 }
