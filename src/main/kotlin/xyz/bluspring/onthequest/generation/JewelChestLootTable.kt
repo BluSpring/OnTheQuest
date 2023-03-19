07f14608bbb7bfeb3185e3ae9abbea951b9c7401
Naz <bluspringindohq@gmail.com>
2023-03-20 03:10:42 +0800

    Wow, I forgot that.

--- src/main/kotlin/xyz/bluspring/onthequest/generation/JewelChestLootTable.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/generation/JewelChestLootTable.kt
@@ -48,10 +48,7 @@ class JewelChestLootTable : LootTable {
         // Fill the chest with obsidian, diamonds and/or gold.
         run {
             for (i in 0..randomGen.nextInt(4, 18)) {
-                if (randomGen.nextInt(5) == 0)
-                    continue
-
-                val count = randomGen.nextInt(1, 9)
+                val count = randomGen.nextInt(0, 9)
 
                 items.add(ItemStack(materials.random(randomGen), count))
             }
