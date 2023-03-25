d5a533a3fb6e33c5c32f1174f394293680ba93a5
Naz <bluspringindohq@gmail.com>
2023-03-26 00:52:55 +0800

    Add Ancient Debris to the items

--- src/main/kotlin/xyz/bluspring/onthequest/generation/JewelChestLootTable.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/generation/JewelChestLootTable.kt
@@ -20,7 +20,8 @@ class JewelChestLootTable : LootTable {
             Material.GOLD_NUGGET,
             Material.IRON_INGOT,
             Material.IRON_NUGGET,
-            Material.EMERALD
+            Material.EMERALD,
+            Material.ANCIENT_DEBRIS
         )
     }
 
@@ -58,9 +59,13 @@ class JewelChestLootTable : LootTable {
         // Fill the chest with obsidian, diamonds and/or gold.
         run {
             for (i in 0..randomGen.nextInt(7, 23)) {
-                val count = randomGen.nextInt(0, 9)
+                val item = materials.random(randomGen)
+                val count = if (item == Material.ANCIENT_DEBRIS)
+                    randomGen.nextInt(0, 5)
+                else
+                    randomGen.nextInt(0, 9)
 
-                items.add(ItemStack(materials.random(randomGen), count))
+                items.add(ItemStack(item, count))
             }
         }
 
