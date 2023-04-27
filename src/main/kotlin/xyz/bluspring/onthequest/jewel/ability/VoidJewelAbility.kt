124b3dd40560c45044dc653ebd8955dd56c6ab47
Naz <bluspringindohq@gmail.com>
2023-04-27 11:17:02 +0800

    Increase damage to wither skull

--- src/main/kotlin/xyz/bluspring/onthequest/jewel/ability/VoidJewelAbility.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/jewel/ability/VoidJewelAbility.kt
@@ -3,7 +3,11 @@ package xyz.bluspring.onthequest.jewel.ability
 import org.bukkit.entity.Player
 import org.bukkit.entity.WitherSkull
 import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.EntityDamageByEntityEvent
 import org.bukkit.event.player.PlayerInteractEvent
+import org.bukkit.metadata.MetadataValue
+import org.bukkit.plugin.Plugin
+import xyz.bluspring.onthequest.OnTheQuest
 import kotlin.time.Duration.Companion.minutes
 
 class VoidJewelAbility : JewelAbility(
@@ -24,10 +28,65 @@ class VoidJewelAbility : JewelAbility(
         }
     }
 
+    @EventHandler
+    fun onPlayerDamagedBySkull(ev: EntityDamageByEntityEvent) {
+        if (ev.damager !is WitherSkull)
+            return
+
+        if (ev.damager.hasMetadata("otq_isPlayerFired")) {
+            ev.damage += 3
+        }
+    }
+
     override fun runAbility(player: Player): Boolean {
-        // TODO: fire wither skull
-        // 30s effect
-        player.launchProjectile(WitherSkull::class.java).velocity = player.location.direction.multiply(2)
+        val skull = player.launchProjectile(WitherSkull::class.java)
+        skull.setMetadata("otq_isPlayerFired", object : MetadataValue {
+            override fun value(): Any {
+                return true
+            }
+
+            override fun asInt(): Int {
+                TODO("Not yet implemented")
+            }
+
+            override fun asFloat(): Float {
+                TODO("Not yet implemented")
+            }
+
+            override fun asDouble(): Double {
+                TODO("Not yet implemented")
+            }
+
+            override fun asLong(): Long {
+                TODO("Not yet implemented")
+            }
+
+            override fun asShort(): Short {
+                TODO("Not yet implemented")
+            }
+
+            override fun asByte(): Byte {
+                TODO("Not yet implemented")
+            }
+
+            override fun asBoolean(): Boolean {
+                return true
+            }
+
+            override fun asString(): String {
+                TODO("Not yet implemented")
+            }
+
+            override fun getOwningPlugin(): Plugin {
+                return OnTheQuest.plugin
+            }
+
+            override fun invalidate() {
+                TODO("Not yet implemented")
+            }
+
+        })
+        skull.velocity = player.location.direction.multiply(2)
 
         return true
     }
