727cc6a3eb55e479fdc3ea441f442e546a012764
Naz <bluspringindohq@gmail.com>
2023-06-28 07:34:04 +0800

    Add custom icon system

---
+++ src/main/kotlin/xyz/bluspring/onthequest/data/util/CooldownUtil.kt
@@ -0,0 +1,28 @@
+package xyz.bluspring.onthequest.data.util
+
+import kotlin.time.Duration.Companion.milliseconds
+
+object CooldownUtil {
+    fun getSubscriptOfNumber(num: Int): String {
+        var text = ""
+
+        num.toString().forEach {
+            text += Char(2070 + it.digitToInt())
+        }
+
+        return text
+    }
+
+    fun getTimeString(ms: Long): String {
+        val duration = ms.milliseconds
+        return "${zeroPad(duration.inWholeMinutes - (duration.inWholeHours * 60))}:${
+            zeroPad(duration.inWholeSeconds - (duration.inWholeMinutes * 60))}"
+    }
+
+    private fun zeroPad(num: Long): String {
+        return if (num <= 9)
+            "0$num"
+        else
+            "$num"
+    }
+}
