8892be49b42b9711a83f76ddf4c4b0a7b14fe981
Naz <bluspringindohq@gmail.com>
2023-06-30 16:19:33 +0800

    Implement quest card system

--- src/main/kotlin/xyz/bluspring/onthequest/data/quests/QuestManager.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/data/quests/QuestManager.kt
@@ -1,8 +1,12 @@
 package xyz.bluspring.onthequest.data.quests
 
 import com.google.gson.JsonObject
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
 import net.minecraft.advancements.Advancement
 import net.minecraft.resources.ResourceLocation
+import org.bukkit.Material
+import org.bukkit.inventory.ItemStack
 import xyz.bluspring.onthequest.data.jewel.Jewel
 
 object QuestManager {
@@ -30,5 +34,34 @@ object QuestManager {
 
     fun isAdvancementAQuest(advancement: Advancement, jewel: Jewel): Boolean {
         return this.questsByAll.contains(advancement.id) || this.questsByJewel[jewel.id]?.contains(advancement.id) == true
+    }
+
+    fun getQuestCard(count: Int): ItemStack {
+        val stack = ItemStack(Material.PAPER, count)
+
+        val meta = stack.itemMeta
+
+        meta.setCustomModelData(1)
+        meta.displayName(Component.text("Quest Card").decoration(TextDecoration.ITALIC, false))
+
+        stack.itemMeta = meta
+
+        return stack
+    }
+
+    fun isQuestCard(stack: ItemStack): Boolean {
+        return stack.hasItemMeta() && stack.type == Material.PAPER && stack.itemMeta.hasCustomModelData() && stack.itemMeta.customModelData == 1
+    }
+
+    fun getAllPossibleQuests(jewel: Jewel): List<ResourceLocation> {
+        val quests = mutableListOf<ResourceLocation>()
+
+        if (questsByJewel.contains(jewel.id)) {
+            quests.addAll(questsByJewel[jewel.id]!!)
+        }
+
+        quests.addAll(questsByAll)
+
+        return quests
     }
 }
--- src/main/kotlin/xyz/bluspring/onthequest/events/QuestEventHandler.kt
+++ src/main/kotlin/xyz/bluspring/onthequest/events/QuestEventHandler.kt
@@ -1,12 +1,21 @@
 package xyz.bluspring.onthequest.events
 
+import net.kyori.adventure.text.Component
 import net.minecraft.resources.ResourceLocation
+import org.bukkit.Bukkit
+import org.bukkit.ChatColor
+import org.bukkit.NamespacedKey
 import org.bukkit.craftbukkit.v1_19_R1.entity.CraftPlayer
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.event.world.LootGenerateEvent
+import xyz.bluspring.onthequest.data.jewel.JewelManager
 import xyz.bluspring.onthequest.data.quests.QuestCustomCriterias
+import xyz.bluspring.onthequest.data.quests.QuestManager
 
 class QuestEventHandler : Listener {
     @EventHandler
@@ -19,5 +28,53 @@ class QuestEventHandler : Listener {
 
         val player = (ev.entity as CraftPlayer).handle
         QuestCustomCriterias.PLAYER_OPEN_NEW_LOOT_CHEST.trigger(player, ResourceLocation(ev.lootTable.key.toString()))
+    }
+
+    @EventHandler
+    fun onPlayerDeath(ev: PlayerDeathEvent) {
+        if (ev.isCancelled)
+            return
+
+        if (ev.player.lastDamageCause == null)
+            return
+
+        if (ev.player.lastDamageCause !is EntityDamageByEntityEvent)
+            return
+
+        if ((ev.player.lastDamageCause!! as EntityDamageByEntityEvent).damager !is Player)
+            return
+
+        ev.drops.add(QuestManager.getQuestCard(1))
+    }
+
+    @EventHandler
+    fun onUseQuestCard(ev: PlayerInteractEvent) {
+        if (!ev.hasItem())
+            return
+
+        if (ev.item == null)
+            return
+
+        if (!QuestManager.isQuestCard(ev.item!!))
+            return
+
+        val quests = QuestManager.getAllPossibleQuests(JewelManager.getOrCreateJewel(ev.player))
+            .filter {
+                !ev.player.getAdvancementProgress(Bukkit.getAdvancement(NamespacedKey.fromString(it.toString())!!)!!).isDone
+                        && !ev.player.scoreboardTags.contains("otq_${it.path.replace("/", "_")}")
+            }
+
+        if (quests.isEmpty()) {
+            ev.player.sendMessage("${ChatColor.RED} >> It appears that we are currently out of quests for you to complete, please try again later!")
+            return
+        }
+
+        val questId = quests.random()
+        val advancement = Bukkit.getAdvancement(NamespacedKey.fromString(questId.toString())!!)!!
+
+        ev.player.addScoreboardTag("otq_${questId.path.replace("/", "_")}")
+        ev.item!!.amount -= 1
+        ev.player.sendMessage("${ChatColor.YELLOW} >> ${ChatColor.GREEN}New Quest: ")
+        ev.player.sendMessage(Component.text(" - ").append(advancement.displayName()))
     }
 }
